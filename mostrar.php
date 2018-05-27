f1bc4ff36f673c5f442e3dbe4b548d524bd73504
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-05-26 21:03:31 -0600

    Ingreso de datos

--- mostrar.php
+++ mostrar.php
@@ -17,7 +17,72 @@
 				echo "<h3>Correo: cab17781@uvg.edu.gt.</h3>";
 				echo "<h3>Lenguajes: Ingl&eacutes, espanol, C/C++, C#, Java, Python, Pascal, MySQL, Php, Javascript, JQuery. </h3>";
 				echo "</center>";
+			}
+			elif ($integrante == "Alexis Fernando Hengstenberg Chocoj"){
+				echo "<center>";
+				echo "<h1>Alexis Fernando Hengstenberg Chocoj</h1></br>";
+				echo "<img src="alexis.jpg"/>";
+				echo "<h3>Edad: 20.</h3>";
+				echo "<h3>Fecha de nacimiento: 21-01-1998.</h3>";
+				echo "<h3>Lugar de nacimiento: Coban, Alta Verapaz,</h3>";
+				echo "<h3>Domicilio: Pendiente.</h3>";
+				echo "<h3>Telefono: 5906-1822.</h3>";
+				echo "<h3>Correo: hen17699@uvg.edu.gt.</h3>";
+				echo "<h3>Lenguajes: Ingl&eacutes, espanol, Java, Python. </h3>";
+				echo "</center>";
+			}
+			elif ($integrante == "Ana Lucia Hernandez Ordonez"){
+				echo "<center>";
+				echo "<h1>Ana Lucia Hernandez Ordonez</h1></br>";
+				echo "<img src="ana.jpg"/>";
+				echo "<h3>Edad: 19.</h3>";
+				echo "<h3>Fecha de nacimiento: 11-03-1999.</h3>";
+				echo "<h3>Lugar de nacimiento: Guatemala, Guatemala,</h3>";
+				echo "<h3>Domicilio: KM 17.5 CARRETERA A EL SALVADOR, ARRAZOLA 1, FRAIJANES.</h3>";
+				echo "<h3>Telefono: 2812-9917.</h3>";
+				echo "<h3>Correo: her17138@uvg.edu.gt.</h3>";
+				echo "<h3>Lenguajes: Ingl&eacutes, espanol, aleman, C#, C++ Java, Python. </h3>";
+				echo "</center>";
 			}	
+			elif ($integrante == "Ana Lucia Hernandez Ordonez"){
+				echo "<center>";
+				echo "<h1>Ana Lucia Hernandez Ordonez</h1></br>";
+				echo "<img src="ana.jpg"/>";
+				echo "<h3>Edad: 19.</h3>";
+				echo "<h3>Fecha de nacimiento: 11-03-1999.</h3>";
+				echo "<h3>Lugar de nacimiento: Guatemala, Guatemala,</h3>";
+				echo "<h3>Domicilio: KM 17.5 CARRETERA A EL SALVADOR, ARRAZOLA 1, FRAIJANES.</h3>";
+				echo "<h3>Telefono: 2812-9917.</h3>";
+				echo "<h3>Correo: her17138@uvg.edu.gt.</h3>";
+				echo "<h3>Lenguajes: Ingl&eacutes, espanol, aleman, C#, C++ Java, Python. </h3>";
+				echo "</center>";
+			}
+			elif ($integrante == "Kevin Sebastian Macario Xiloj"){
+				echo "<center>";
+				echo "<h1>Kevin Sebastian Macario Xiloj</h1></br>";
+				echo "<img src="kevin.jpg"/>";
+				echo "<h3>Edad: 18.</h3>";
+				echo "<h3>Fecha de nacimiento: 10-07-1999.</h3>";
+				echo "<h3>Lugar de nacimiento: Guatemala, Guatemala,</h3>";
+				echo "<h3>Domicilio: 5A AVENIDA B 8-61 ZONA 3 DE MIXCO, NUEVA MONTSERRAT.</h3>";
+				echo "<h3>Telefono: 4215-9325.</h3>";
+				echo "<h3>Correo: kevinmacario.e@gmail.com.</h3>";
+				echo "<h3>Lenguajes: Ingl&eacutes, espanol, C#, Java, Python. </h3>";
+				echo "</center>";
+			}
+			elif ($integrante == "Raul Alejandro Monzon Solis"){
+				echo "<center>";
+				echo "<h1>Raul Alejandro Monzon Solis</h1></br>";
+				echo "<img src="raul.jpg"/>";
+				echo "<h3>Edad: 19.</h3>";
+				echo "<h3>Fecha de nacimiento: 06-07-1998.</h3>";
+				echo "<h3>Lugar de nacimiento: Guatemala, Guatemala,</h3>";
+				echo "<h3>Domicilio: 16 CALLE 38-18 ZONA 5 JARDINES DE LA ASUNCION SUR.</h3>";
+				echo "<h3>Telefono: 5017-1741.</h3>";
+				echo "<h3>Correo: mon17014@uvg.edu.gt.</h3>";
+				echo "<h3>Lenguajes: Ingl&eacutes, espanol, aleman, C/C++, Pascal, Perl C#, Java, Python. </h3>";
+				echo "</center>";
+			}			
 		?>
 	</body>
 </html>
